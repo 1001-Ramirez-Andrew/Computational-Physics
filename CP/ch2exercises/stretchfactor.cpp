09943342a78f29d9d95dfc64de5e6578e561e0b4
Andrew Ramirez <andrewramirez2566@gmail.com>
2021-12-31 16:08:38 -0800

    12/31/2021 changes

--- CP/ch2exercises/stretchfactor.cpp
+++ CP/ch2exercises/stretchfactor.cpp
@@ -2,22 +2,43 @@
 #include <cmath>
 #include <limits>
 #include <iomanip>
+#include <sstream>
+#include <string>
 
-double betaStretch(double beta);
-double epsilonStretch(double epsilon);
+long double betaStretch(double beta);
+long double epsilonStretch(double epsilon);
 
 int main(){
-    std::cout << std::numeric_limits<double>::epsilon() << std::endl;
-    double weird = 1.0;
-    weird += 3.27047E-14;
-    std::cout << std::setprecision(25) << weird << std::endl;
+    //keep in mind the machine numbers have an error associated with them on top of the error from the operation, for this purpose we assume that the machine represestations are exact.
+    
+    long double e = .1;
+    long double b = .9;
+    long double beta = 0;
+    long double epsilon = 0;
+
+    std::string bString = "0.9";
+
+    do{
+        beta = betaStretch(b);
+        epsilon = epsilonStretch(e);
+
+        bString += "9";
+        std::istringstream bStream(bString);
+        bStream >> b;
+        std::cout << std::setprecision(23) << b << std::endl;
+        
+        e *= .1;
+    }
+    while(abs(beta - epsilon)/epsilon < .001);
+    std::cout <<  "Maxiumum beta is: " << std::setprecision(23) << b << std::endl;
+
     return 0;
 }
 
-double betaStretch(double beta){
+long double betaStretch(double beta){
     return 1/sqrt(1-beta*beta);
 }
 
-double epsilonStretch(double epsilon){
+long double epsilonStretch(double epsilon){
     return 1/sqrt((2-epsilon)*epsilon);
 }
